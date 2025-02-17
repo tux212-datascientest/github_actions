4c102a8ed4520779759f619f7d59fe23a8026e29
Vincent L <vincent.lefort@amue.fr>
2025-02-17 22:01:39 +0100

    ./file_test.py:9:3: E114 indentation is not a multiple of 4 (comment)

--- file_test.py
+++ file_test.py
@@ -1,12 +1,12 @@
 import pytest
 
 def test_calc_addition():
-    # Fonction test du résultat de 2+4
+# Fonction test du résultat de 2+4
     output = 2+4
     assert output == 6
 
 def test_calc_substraction():
-  # Fonction test du résultat de 2-4
+    # Fonction test du résultat de 2-4
     output = 2-4
     assert output == -2
 
